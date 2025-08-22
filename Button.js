1001e65ba962db4fd71cf14d8638ef04be37dbd7
Adila Safitri <jiads1207@gmail.com>
2025-08-22 21:13:50 +0700

    Add files via upload

---
+++ Button.js
@@ -0,0 +1,3 @@
+function username(){
+            alert("Nama Lengkap: Adila Safitri \n Hobi: Fangirling \n Umur: 16 \n Nama Sekolah: Smk Media Karya \n TTL: Jakarta 05 Oktober 2008 ");
+        }
---
+++ div_id.js
@@ -0,0 +1,6 @@
+//membuat objek elemen
+      var nama = document . getElementById("Tujuan");
+      
+      // menampilkan output ke elemen hasil
+      nama.innerHTML = "<p>keinginan :  Cepet dapet kerja biar ketemu Jihoon <br>Hobi : Menyanyi and Menari <br>Tujuan Hidup : Sukses biar bisa undang Jihoon ke wedding </p>";
+      
